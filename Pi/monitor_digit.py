ca64ff8c96c33a14a5a89e6e2e8c71a2e69e1c1f
x1001000 <x1001000@gmail.com>
2018-02-20 00:07:31 +0800

    from Mac

---
+++ Pi/monitor_digit.py
@@ -0,0 +1,25 @@
+import io, picamera, readchar
+from PIL import Image
+import matplotlib.image as mpimg
+import numpy as np
+from keras.models import load_model
+model = load_model('CNN_model.h5')
+stream = io.BytesIO()                   # Create the in-memory stream
+with picamera.PiCamera() as camera:
+    camera.resolution = (896,896)
+    camera.start_preview()
+    while True:
+        if readchar.readkey() == 'q':
+            break
+        camera.capture(stream, format='jpeg')
+        stream.seek(0)
+        with Image.open(stream) as im:
+            im = im.convert('L')
+            im.thumbnail((28,28))
+            im.save('28x28.png')
+        img = mpimg.imread('28x28.png')
+        img = img.reshape(1,28,28,1).astype('float32')
+        img = np.ones(img.shape) - img
+        prediction = model.predict_classes(img)
+        print('Handwritten',i,'is predicted as',prediction[0])
+    camera.stop_preview()
