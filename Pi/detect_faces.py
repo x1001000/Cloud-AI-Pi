ccc7ae82bfba7ffea7287d99ed60cd4364b20623
x1001000 <x1001000@gmail.com>
2018-02-20 21:13:14 +0800

    spin time = 0.95s

--- Pi/detect_faces.py
+++ Pi/detect_faces.py
@@ -14,6 +14,6 @@ with picamera.PiCamera() as camera:
         highlight.highlight(faces, 'faces.jpg')
         print('faces_highlight.jpg created!')
         for _ in range(len(faces)):
-            control.move('k',0.9)
+            control.move('k',0.95)
             sleep(0.5)
     control.GPIO.cleanup()
