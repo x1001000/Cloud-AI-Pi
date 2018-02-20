f134ad54c819b9b34ba29b5fe03aaeef51d5b5fc
x1001000 <x1001000@gmail.com>
2018-02-20 12:05:05 +0800

    faces.jpg

---
+++ Pi/detect_faces.py
@@ -0,0 +1,14 @@
+import picamera, readchar
+from lib import detect, hightlight
+with picamera.PiCamera() as camera:
+    camera.resolution = (1920,1920)
+    camera.hflip = True
+    camera.vflip = True
+    camera.brightness = 100
+    camera.contrast = 100
+    while readchar.readkey() != 'q':
+        camera.capture('faces.jpg')
+        faces = detect.faces('faces.jpg')
+        print(len(faces), 'face(s) found!')
+        highlight.highlight(faces, 'faces.jpg')
+        print(PATH[:-4]+'-out'+PATH[-4:], 'created!')
