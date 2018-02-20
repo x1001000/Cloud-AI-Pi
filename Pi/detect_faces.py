d27e8750edb9868e78c4c79a2bf333ef73f7db9c
x1001000 <x1001000@gmail.com>
2018-02-20 13:10:23 +0800

    contrast=0 brightness=50

--- Pi/detect_faces.py
+++ Pi/detect_faces.py
@@ -4,8 +4,8 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1920,1920)
     camera.hflip = True
     camera.vflip = True
-    camera.brightness = 100
-    camera.contrast = 100
+    camera.contrast = 0
+    camera.brightness = 50
     while input('ENTER to detect, else to quit...') == '':
         camera.capture('faces.jpg')
         faces = detect.faces('faces.jpg')
