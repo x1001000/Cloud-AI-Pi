26b1b9b47f22a1013fd5dae22863403d4bc7bb91
十百千 <x1001000@users.noreply.github.com>
2019-08-15 15:57:39 +0800

    Update detect_faces.py

--- Pi/detect_faces.py
+++ Pi/detect_faces.py
@@ -12,13 +12,13 @@ with picamera.PiCamera() as camera:
         faces = detect.faces('faces.jpg')
         print(len(faces), 'face(s) found!')
         highlight.highlight(faces, 'faces.jpg')
-        print('faces-out.jpg created!')
+        #print('faces-out.jpg created!')
         happy_faces = 0
         for face in faces:
             if detect.happy(face) == True:
                 happy_faces += 1
         if happy_faces:
-            print(f'There are {happy_faces} happy faces!')
+            print(f'There are {happy_faces} happy face(s)!')
             for _ in range(happy_faces):
                 control.move(sys.argv[1],float(sys.argv[2]))
                 sleep(0.5)
